77e4722565f51962974f64eb6db2df7b08a25cd3
shangfenghuang <1261717268@qq.com>
2023-08-11 15:56:45 -0600

    11/8

---
+++ datasets/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+# @Time    : 2023-08-11 2:41 p.m.
+# @Author  : shangfeng
+# @Organization: University of Calgary
+# @File    : __init__.py.py
+# @IDE     : PyCharm
+
+
+def build_dataset(args):
---
+++ datasets/tallinn.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+# @Time    : 2023-08-11 3:06 p.m.
+# @Author  : shangfeng
+# @Organization: University of Calgary
+# @File    : tallinn.py.py
+# @IDE     : PyCharm
+
+import os
+import sys
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class Building3DReconstructionDataset(Dataset):
+    def __init__(self, data, targets, transform=None):
+        self.data = data
+        self.targets = targets
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.targets[index]
+
+        if self.transform:
+            x = self.transform(x)
+
+        return x, y
